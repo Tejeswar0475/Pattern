349e53d9edb1c2d31e7adf360f7780b017ce5af4
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-25 21:47:33 +0530

    Added a First and Last Name

--- pattern.sh
+++ pattern.sh
@@ -8,3 +8,12 @@ then
 else
         echo "No pattern matched";
 fi
+
+read -p "Enter First name and last Name:" userFLName
+pattern2="(^[A-Z]{1}[a-z]{2,}\s[A-Z]{1}[a-z]{2,}$)"
+if [[ $userFLName =~ $pattern2 ]]
+then
+        echo "Pattern matched"
+else
+        echo "Pattern not matched"
+fi
