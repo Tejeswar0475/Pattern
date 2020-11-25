0c5fccf109c9778e637f23c162809ce4548fd616
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-25 21:20:05 +0530

    Added regexp for FirstName

--- pattern.sh
+++ pattern.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+read -p "Enter your name:" userFName
+pattern="(^[A-Z]{1})[a-z]{2,}$"
+if [[ $userFName =~ $pattern ]]
+then
+        echo "Yes pattern matched";
+else
+        echo "No pattern matched";
+fi
